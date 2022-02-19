647af70ae23b520dc75ce6431a75310286450bce
BananinBanano <97204399+BananinBanano@users.noreply.github.com>
2022-02-19 14:37:34 +0100

    Added broadcast

--- Config.cs
+++ Config.cs
@@ -10,5 +10,11 @@ namespace PeanutInfection
 
         [Description("How many seconds before the player turns into SCP-173")]
         public float Delay { get; set; } = 2f;
+        
+        [Description("Enable broadcast when the player turns into SCP-173")]
+        public bool Enablebc { get: set; } = true;
+        
+        [Description("Configurable BC")]
+        public string Broadcast { get; set; } = "You have beenn infected by <color=#ff0000>SCP-173</color>!";
     }
 }
